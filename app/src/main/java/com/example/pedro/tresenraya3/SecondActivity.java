f8f3634e3a20fc456febb969695feb30fab6813c
likytho <killershare8295@gmail.com>
2015-03-12 18:59:26 +0100

    Initial commit: creadas dos activiies

---
+++ app/src/main/java/com/example/pedro/tresenraya3/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.example.pedro.tresenraya3;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Pedro on 12/03/2015.
+ */
+public class SecondActivity extends ActionBarActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+
+
+    }
+}
