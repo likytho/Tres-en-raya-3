63cda6eb0c65f08c31171efa2bb95dcdaab83c5a
likytho <killershare8295@gmail.com>
2015-03-18 18:30:10 +0100

    Añadida x, e intento incompleto de modificación del color de fondo

--- app/src/main/java/com/example/pedro/tresenraya3/SecondActivity.java
+++ app/src/main/java/com/example/pedro/tresenraya3/SecondActivity.java
@@ -1,8 +1,13 @@
 package com.example.pedro.tresenraya3;
 
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by Pedro on 12/03/2015.
@@ -14,6 +19,26 @@ public class SecondActivity extends ActionBarActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
 
+        ImageButton equis =(ImageButton) findViewById(R.id.imageButton);
 
     }
+
+    public void onClick(View view){
+        //ColorDrawable fondo = (ColorDrawable) view.getBackground();
+        //int colorId = fondo.getColor();
+
+        //ImageButton equis =(ImageButton) findViewById(R.id.imageButton);
+
+       // view.setBackgroundColor(getResources().getColor(R.color.Green));
+
+       // Resources res = getResources();
+
+        if(view.getDrawingCacheBackgroundColor() == R.color.Green){
+            view.setBackgroundColor(getResources().getColor(R.color.Blue));
+        }else{
+            view.setBackgroundColor(getResources().getColor(R.color.Green));
+        }
+    }
+
+
 }
